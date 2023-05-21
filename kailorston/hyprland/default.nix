43bbd31edbf867317ebb59b370ce55ec100f86c0
ShubhamBhut <shubhamprecious1@gmail.com>
2023-05-21 20:04:26 +0530

    waybar config

--- kailorston/hyprland/default.nix
+++ kailorston/hyprland/default.nix
@@ -1,4 +1,19 @@
-{ config, lib, pkgs, inputs, ... }:
-{
-  imports = [ ./hyprland_waybar.nix ];
+{ pkgs, lib, ... }: {
+ home.packages = with pkgs; [ wl-clipboard slurp grim rofi brightnessctl ];
+  programs.waybar.enable = true;
+  programs.waybar.style = ./waybar.css;
+  programs.waybar.settings = {
+    "bar-0" = {
+      ipc = true;
+      layer = "top";
+      position = "top";
+      height = 22;
+      modules-left = [ "sway/workspaces" "sway/mode" ];
+      modules-right = [ "battery" "clock" "tray" ];
+      "sway/workspaces" = {
+        disable-scroll = true;
+        all-outputs = true;
+      };
+    };
+  };
 }
