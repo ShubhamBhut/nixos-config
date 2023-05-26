0727e2d9c8234ef72e6f5fe20ca5b2eae75da924
ShubhamBhut <shubhamprecious1@gmail.com>
2023-05-26 17:58:43 +0530

    rust addition

--- kailorston/dev.nix
+++ kailorston/dev.nix
@@ -4,6 +4,8 @@
   home.packages = with pkgs; [
     gcc
     gdb
+    rustc
+    cargo
     (import ./python-packages.nix { pkgs = pkgs; })
   ];
   home.sessionVariables.CARGO_BUILD_TARGET_DIR = "${config.home.homeDirectory}/.cargo/target";
