eab2bf681aca4eedd71c9d1b955b8a1b7d09dc93
ShubhamBhut <shubhamprecious1@gmail.com>
2023-05-30 15:28:48 +0530

    neo4j removal and helix addition

--- kailorston/dev.nix
+++ kailorston/dev.nix
@@ -6,9 +6,16 @@
     gdb
     rustc
     cargo
-    neo4j
-    neo4j-desktop
     (import ./python-packages.nix { pkgs = pkgs; })
   ];
+  programs.helix.enable = true;
+  programs.helix.settings = {
+    theme = "github_dark";
+    keys.insert.F9 = "normal_mode";
+    keys.normal.a = "code_action";
+    keys.normal.space.s = ":write";
+    editor.cursor-shape.insert = "bar";
+    editor.line-number = "relative";
+  };
   home.sessionVariables.CARGO_BUILD_TARGET_DIR = "${config.home.homeDirectory}/.cargo/target";
   }
