bf6d511bb6d0353b4e020ea6c2c8cabd4c8b0e08
ShubhamBhut <shubhamprecious1@gmail.com>
2023-06-08 08:56:21 +0530

    nvim harpoon and undotree addition

--- kailorston/nvim/lua/plugins/main.lua
+++ kailorston/nvim/lua/plugins/main.lua
@@ -32,6 +32,28 @@ return {
       'norcalli/nvim-colorizer.lua',
       opts = {},
    },
+   {
+      'theprimeagen/harpoon',
+      opts = {},
+      config = function(_, opts)
+      local mark = require("harpoon.mark")
+      local ui = require("harpoon.ui")
+
+      vim.keymap.set("n", "<leader>a", mark.add_file)
+      vim.keymap.set("n", "<leader>h", ui.toggle_quick_menu)
+
+      vim.keymap.set("n", "<C-h>", function() ui.nav_file(1) end)
+      vim.keymap.set("n", "<C-j>", function() ui.nav_file(2) end)
+      vim.keymap.set("n", "<C-k>", function() ui.nav_file(3) end)
+      vim.keymap.set("n", "<C-l>", function() ui.nav_file(4) end)
+        end,
+   },
+   {
+      'mbbill/undotree',
+      config = function(_, opts)
+      vim.keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle)
+      end,
+   },
 
    {
       "chrisgrieser/nvim-various-textobjs",
