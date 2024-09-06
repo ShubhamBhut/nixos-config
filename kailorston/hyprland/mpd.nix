8586cdb368ae094897a685dc243cdbdf41fe935d
ShubhamBhut <shubhamprecious1@gmail.com>
2024-09-06 10:49:04 +0530

    mpd addition

--- kailorston/hyprland/mpd.nix
+++ kailorston/hyprland/mpd.nix
@@ -1,10 +1,23 @@
 services.mpd = {
   enable = true;
-  musicDirectory = "/path/to/music";
+  musicDirectory = "./Music/";
   extraConfig = ''
     # must specify one or more outputs in order to play audio!
     # (e.g. ALSA, PulseAudio, PipeWire), see next sections
-  '';
+    audio_output {
+      type            "pipewire"
+      name            "PipeWire Sound Server"
+    }
 
+    audio_output {
+      type            "pulse"
+      name            "PulseAudio"
+    }
+
+    bind_to_address "127.0.0.1"
+    port "6600"
+
+    auto_update "yes"
+  '';
 };
 
